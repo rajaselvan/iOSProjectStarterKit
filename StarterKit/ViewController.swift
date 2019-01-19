7403ba958f1d5a20d721921d5ebcb14e6d61452c
pandiyaraji2i <pandiyaraj@ideas2it.com>
2019-01-19 13:57:08 +0530

    Initial Commit

---
+++ StarterKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarterKit
+//
+//  Created by Rajaselvan Thangaraj on 19/01/19.
+//  Copyright © 2019 Rajaselvan Thangaraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
